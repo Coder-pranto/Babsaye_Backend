0e894264f803c34e297914c5815179548c479ac7
Riaj Hasan Pranto <prantocse18@gmail.com>
2024-08-24 17:01:51 +0600

    updated

--- controllers/supplierGroupController.js
+++ controllers/supplierGroupController.js
@@ -68,7 +68,7 @@ const deleteSupplierGroup = async (req, res) => {
       return res.status(404).json({ message: 'Supplier group not found' });
     }
 
-    await group.remove();
+    await group.deleteOne();
     res.status(200).json({ message: 'Supplier group deleted successfully' });
   } catch (error) {
     res.status(400).json({ message: error.message });
